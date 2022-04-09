43d6abc29d1208257d4630868c920d4370f445a5
mariannabaldez <marianna.baldez@gmail.com>
2022-04-09 12:53:15 -0300

    renderizando pagina home

---
+++ my-app/pages/index.js
@@ -0,0 +1,10 @@
+import App from "next/app";
+import Head from 'next/head'
+
+import Home from '../components/cabecalho-e-rodape'
+
+export default function Index(){
+    return (
+        <Home />
+    );
+};
