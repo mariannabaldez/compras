5052bda42c29e7cd5d1feeb4ac400ec7113f3880
mariannabaldez <marianna.baldez@gmail.com>
2022-04-15 15:08:12 -0300

    .

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 from fastapi import FastAPI
-from models.schemas.produto import Produto
-from db.lista_de_compras import lista_de_compras
+#from models.schemas.produto import Produto
+#from db.lista_de_compras import lista_de_compras
 
 from api.routers.app import *
