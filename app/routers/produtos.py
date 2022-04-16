c77441695c93b0a0c30aa2ef2758ce804957b3f3
mariannabaldez <marianna.baldez@gmail.com>
2022-04-16 15:31:10 -0300

    reestruturação projeto

--- app/routers/produtos.py
+++ app/routers/produtos.py
@@ -1,29 +1,29 @@
-from fastapi import APIRouter, status
+from fastapi import FastAPI, status
 from models.schemas.produto import Produto
 from db.lista_de_compras import lista_de_compras
 
-router = APIRouter(tags=['Produtos'])
+app = FastAPI()
 
 
-@router.get("/")
+@app.get("/")
 async def visualizar():
     return lista_de_compras
 
-@router.get("/{item}")
+@app.get("/{item}")
 async def visualizar_elemento(item):
     return lista_de_compras[item]
 
-@router.post("/", response_model=dict, status_code=status.HTTP_201_CREATED)
+@app.post("/", response_model=dict, status_code=status.HTTP_201_CREATED)
 async def adiciona(produto: str, link: str):
     lista_de_compras[produto] = dict()
     lista_de_compras[produto]["link"] = link
     return lista_de_compras
 
-@router.delete("/")
+@app.delete("/")
 async def deleta(produto: str):
     return lista_de_compras.pop(produto, False)
 
-@router.patch("/")
+@app.patch("/")
 async def altera(novos_valores: Produto, produto: str):
     # Acessa produto dentro do "banco de dados" e preenche com novos_valores
     lista_de_compras[produto] = {**novos_valores.dict()} # a var produto - não é a key e sim o value que estaa associado a key
