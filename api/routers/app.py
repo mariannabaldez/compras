f01e45cea396b4aa851ed8ed9ed8ccbcaba2514b
mariannabaldez <marianna.baldez@gmail.com>
2022-04-16 15:09:14 -0300

    .

--- api/routers/app.py
+++ api/routers/app.py
@@ -1,29 +1,29 @@
-from fastapi import FastAPI, status
+from fastapi import APIRouter, status
 from models.schemas.produto import Produto
 from db.lista_de_compras import lista_de_compras
 
-app = FastAPI()
+router = APIRouter(tags=['Produtos'])
 
 
-@app.get("/")
+@router.get("/")
 async def visualizar():
     return lista_de_compras
 
-@app.get("/{item}")
+@router.get("/{item}")
 async def visualizar_elemento(item):
     return lista_de_compras[item]
 
-@app.post("/", response_model=dict, status_code=status.HTTP_201_CREATED)
+@router.post("/", response_model=dict, status_code=status.HTTP_201_CREATED)
 async def adiciona(produto: str, link: str):
     lista_de_compras[produto] = dict()
     lista_de_compras[produto]["link"] = link
     return lista_de_compras
 
-@app.delete("/")
+@router.delete("/")
 async def deleta(produto: str):
     return lista_de_compras.pop(produto, False)
 
-@app.patch("/")
+@router.patch("/")
 async def altera(novos_valores: Produto, produto: str):
     # Acessa produto dentro do "banco de dados" e preenche com novos_valores
     lista_de_compras[produto] = {**novos_valores.dict()} # a var produto - não é a key e sim o value que estaa associado a key
--- main.py
+++ main.py
@@ -2,4 +2,7 @@ from fastapi import FastAPI
 #from models.schemas.produto import Produto
 #from db.lista_de_compras import lista_de_compras
 
-from api.routers.app import *
+from api.routers.app import router
+
+app = FastAPI()
+app.include_router(router)
