749c72b2ae0955b1c546c1881bd24676f6161ed7
mariannabaldez <marianna.baldez@gmail.com>
2022-04-17 22:41:33 -0300

    rota patch alterada para acessar bd

--- app/routers/produtos.py
+++ app/routers/produtos.py
@@ -8,9 +8,11 @@ router = APIRouter(prefix="/produtos")
 async def visualizar():
     return lista_de_compras
 
+
 @router.get("/{item}")
 async def visualizar_elemento(item):
     return lista_de_compras[item]
+
 
 @router.post("/", response_model=dict, status_code=status.HTTP_201_CREATED)
 async def adiciona(produto: Produto):
@@ -18,14 +20,17 @@ async def adiciona(produto: Produto):
     id = await database.execute(instrucao)
     return {id: produto}
 
+
 @router.delete("/", response_model=str, status_code=status.HTTP_200_OK)
 async def deleta(id: int):
     instrucao = tabela_produtos.delete().where(tabela_produtos.c.id==id)
     id = await database.execute(instrucao)
     return f"Produto com id: {id} apagado"
 
+
 @router.patch("/")
-async def altera(novos_valores: Produto, produto: str):
+async def altera(id: int, novos_valores: Produto):
     # Acessa produto dentro do "banco de dados" e preenche com novos_valores
-    lista_de_compras[produto] = {**novos_valores.dict()} # a var produto - não é a key e sim o value que estaa associado a key
-    return lista_de_compras
+    instrucao = tabela_produtos.update().where(tabela_produtos.c.id==id).values(**novos_valores.dict())
+    id = await database.execute(instrucao)
+    return {id: novos_valores}
