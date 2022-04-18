74c19eebe3a042d26976470bc9ead2831308d97e
mariannabaldez <marianna.baldez@gmail.com>
2022-04-17 22:21:18 -0300

    update pydantic

--- app/models.py
+++ app/models.py
@@ -1,5 +1,6 @@
 from pydantic import BaseModel, conint, constr
 
 class Produto(BaseModel):
+    nome: constr(strip_whitespace=True, to_lower=True)
     link: constr(strip_whitespace=True, to_lower=True)
     quantidade: conint(ge=0)
