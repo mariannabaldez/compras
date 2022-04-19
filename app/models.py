4409dede0d9be041be4a2b79630fd94307476237
mariannabaldez <marianna.baldez@gmail.com>
2022-04-18 22:30:07 -0300

    estrutura

--- app/models.py
+++ app/models.py
@@ -4,3 +4,11 @@ class Produto(BaseModel):
     nome: constr(strip_whitespace=True, to_lower=True)
     link: constr(strip_whitespace=True, to_lower=True)
     quantidade: conint(ge=0)
+
+# model dos parametros das funções:
+class Parametros(BaseModel):
+    pass
+
+# model para retorno das funções de rota:
+class Retorno(BaseModel):
+    pass
