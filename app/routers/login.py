f2ad4ae38d31c9eea494ff5e472f10d8a03c3e9b
mariannabaldez <marianna.baldez@gmail.com>
2022-04-23 11:56:59 -0300

    criadas rotas de login

--- app/routers/login.py
+++ app/routers/login.py
@@ -5,13 +5,24 @@ import sqlalchemy
 
 router = APIRouter(prefix="/login")
 
-@router.post("/", response_model=dict,)
-async def adiciona(login: Login):
+@router.get("/")
+async def visualizar():
     pass
 
+@router.post("/", response_model=dict,)
+async def adiciona(login: Login):
+    instrucao = tabela_login.insert().values(**login.dict())
+    id = await database.execute(instrucao)
+    return {id: login}
 
-#@router.post("/", response_model=dict, status_code=status.HTTP_201_CREATED)
-#async def adiciona(produto: Produto):
-#    instrucao = tabela_produtos.insert().values(**produto.dict())
- #   id = await database.execute(instrucao)
- #   return {id: produto}
+@router.delete("/", status_code=status.HTTP_200_OK)
+async def deleta(id: int):
+    instrucao = tabela_login.delete().where(tabela_login.c.id==id)
+    id = await database.execute(instrucao)
+    return f"Login com id: {id} apagado"
+
+@router.patch("/")
+async def altera(id: int, novos_valores: Login):
+    instrucao = tabela_login.update().where(tabela_login.c.id==id).values(**novos_valores.dict())
+    id = await database.execute(instrucao)
+    return {id: novos_valores}
